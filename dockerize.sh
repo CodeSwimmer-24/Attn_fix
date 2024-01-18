69fa1635143970e04842870b184d6798385dd0eb
Shahilcoder <shahilcoder@gmail.com>
2024-01-18 21:46:40 +0530

    make commands run on superuser privileges

--- dockerize.sh
+++ dockerize.sh
@@ -1,5 +1,5 @@
-docker build -f dockerize/crow/Dockerfile . -t crow:latest
-docker build -f dockerize/server/Dockerfile . -t test:latest
+sudo docker build -f dockerize/crow/Dockerfile . -t crow:latest
+sudo docker build -f dockerize/server/Dockerfile . -t test:latest
 
-docker container prune -f
-docker run --name testc -p 8080:8080 test:latest
+sudo docker container prune -f
+sudo docker run --name testc -p 8080:8080 test:latest
