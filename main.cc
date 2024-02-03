8a5b6b9623c119a4f6a255353bdf60a32adf6a57
Shahilcoder <shahilcoder@gmail.com>
2024-02-03 22:03:53 +0530

    config changes

--- main.cc
+++ main.cc
@@ -5,7 +5,7 @@ int main() {
 #ifdef _WIN32
     drogon::app().loadConfigFile("../../config.json");
 #else
-    drogon::app().loadConfigFile("./config.json");
+    drogon::app().loadConfigFile("../config.json");
 #endif
     
     // drogon::app().registerHandler("/", [](const drogon::HttpRequest& req
@@ -16,6 +16,7 @@ int main() {
     // }, {drogon::Get});
 
     //Run HTTP framework,the method will block in the internal event loop
+    // drogon::app().addListener("0.0.0.0", 5555);
     LOG_INFO << "Server running on 127.0.0.1:5555";
     drogon::app().run();
 
