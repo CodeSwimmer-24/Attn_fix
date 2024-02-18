c63583fdb7a187699d1dffa956ea54c9bdc46b01
Shahilcoder <shahilcoder@gmail.com>
2024-02-18 13:28:52 +0530

    port change

--- scripts/dockerize.sh
+++ scripts/dockerize.sh
@@ -2,4 +2,4 @@ sudo docker build -f ../dockerize/drogon/Dockerfile .. -t drogon:latest
 sudo docker build -f ../dockerize/server/Dockerfile .. -t test:latest
 
 sudo docker container prune -f
-sudo docker run --name testc -p 5555:5555 test:latest
+sudo docker run --name testc -p 8080:80 test:latest
